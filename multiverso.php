47814745a7197d9bba5235fc52bfb372b0659cd3
Eme Arko <marco.palmero2001@gmail.com>
2022-02-16 17:44:22 +0100

    Multiverso Actualizado

--- multiverso.php
+++ multiverso.php
@@ -14,51 +14,24 @@ include('header.php')
 ?>
 </header>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="Imagenes/multiverso/mig2.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="Imagenes/multiverso/mig3.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="Imagenes/multiverso/mig1.png" alt="Third slide">
-    </div>
+<div class="card-group">
+  <div class="card">
+    <img src="Imagenes/multiverso/mig1.png" class="card-img-top" alt="...">
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  <div class="card">
+    <img src="Imagenes/multiverso/mig2.png" class="card-img-top" alt="...">
+  </div>
+  <div class="card">
+    <img src="Imagenes/multiverso/mig3.png" class="card-img-top" alt="...">
+  </div>
 </div>
-
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="Imagenes/multiverso/hdc1.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="Imagenes/multiverso/hdc2.png" alt="Second slide">
-    </div>
+<div class="card-group">
+  <div class="card">
+    <img src="Imagenes/multiverso/hdc1.png" class="card-img-top" alt="...">
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  <div class="card">
+    <img src="Imagenes/multiverso/hdc2.png" class="card-img-top" alt="...">
+  </div>
 </div>
 
 <h5 style="text-align: center;">Creditos a <a href="https://www.instagram.com/_.photos._2003/" target="_blank">Patricia</a> y a <a href="https://www.instagram.com/xaival.photo/" target="_blank">Xaival</a> por las fotos</a>
